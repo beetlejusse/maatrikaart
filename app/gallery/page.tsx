84bb356c39f9b557f33f15323259845bbb54907d
vishalBelwal <vishalbelwal52@gmail.com>
2025-08-12 23:48:36 +0530

    gallery mockup

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 import React from "react";
 import Showcards from "@/components/ShowCards";
+import PaintingsGallery from "../../components/PaintingsManager";
 
 const page = () => {
   return (
     <div>
       <div className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold text-center mb-8 mt-32">प्रदर्शनी</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          <Showcards
+        <div className="">
+          {/* <Showcards
             imageUrl="https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             price="$450,000"
             agent="Liam von Hohenberg..."
@@ -17,127 +18,9 @@ const page = () => {
             rooms={4}
             postedBy="pixenbytayyub"
             postedTime="2 days ago"
-          />
-          <Showcards
-            imageUrl="https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            price="$450,000"
-            agent="Liam von Hohenberg..."
-            agency="2045 AG Lucien Moreau"
-            area="40 m²"
-            rooms={4}
-            postedBy="pixenbytayyub"
-            postedTime="2 days ago"
-          />
-          <Showcards
-            imageUrl="https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            price="$450,000"
-            agent="Liam von Hohenberg..."
-            agency="2045 AG Lucien Moreau"
-            area="40 m²"
-            rooms={4}
-            postedBy="pixenbytayyub"
-            postedTime="2 days ago"
-          />
-          <Showcards
-            imageUrl="https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            price="$450,000"
-            agent="Liam von Hohenberg..."
-            agency="2045 AG Lucien Moreau"
-            area="40 m²"
-            rooms={4}
-            postedBy="pixenbytayyub"
-            postedTime="2 days ago"
-          />
-          <Showcards
-            imageUrl="https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            price="$450,000"
-            agent="Liam von Hohenberg..."
-            agency="2045 AG Lucien Moreau"
-            area="40 m²"
-            rooms={4}
-            postedBy="pixenbytayyub"
-            postedTime="2 days ago"
-          />
-          <Showcards
-            imageUrl="https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            price="$450,000"
-            agent="Liam von Hohenberg..."
-            agency="2045 AG Lucien Moreau"
-            area="40 m²"
-            rooms={4}
-            postedBy="pixenbytayyub"
-            postedTime="2 days ago"
-          />
-          <Showcards
-            imageUrl="https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            price="$450,000"
-            agent="Liam von Hohenberg..."
-            agency="2045 AG Lucien Moreau"
-            area="40 m²"
-            rooms={4}
-            postedBy="pixenbytayyub"
-            postedTime="2 days ago"
-          />
-          <Showcards
-            imageUrl="https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            price="$450,000"
-            agent="Liam von Hohenberg..."
-            agency="2045 AG Lucien Moreau"
-            area="40 m²"
-            rooms={4}
-            postedBy="pixenbytayyub"
-            postedTime="2 days ago"
-          />
-          <Showcards
-            imageUrl="https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            price="$450,000"
-            agent="Liam von Hohenberg..."
-            agency="2045 AG Lucien Moreau"
-            area="40 m²"
-            rooms={4}
-            postedBy="pixenbytayyub"
-            postedTime="2 days ago"
-          />
-          <Showcards
-            imageUrl="https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            price="$450,000"
-            agent="Liam von Hohenberg..."
-            agency="2045 AG Lucien Moreau"
-            area="40 m²"
-            rooms={4}
-            postedBy="pixenbytayyub"
-            postedTime="2 days ago"
-          />
-          <Showcards
-            imageUrl="https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            price="$450,000"
-            agent="Liam von Hohenberg..."
-            agency="2045 AG Lucien Moreau"
-            area="40 m²"
-            rooms={4}
-            postedBy="pixenbytayyub"
-            postedTime="2 days ago"
-          />
-          <Showcards
-            imageUrl="https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            price="$450,000"
-            agent="Liam von Hohenberg..."
-            agency="2045 AG Lucien Moreau"
-            area="40 m²"
-            rooms={4}
-            postedBy="pixenbytayyub"
-            postedTime="2 days ago"
-          />
-          <Showcards
-            imageUrl="https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            price="$450,000"
-            agent="Liam von Hohenberg..."
-            agency="2045 AG Lucien Moreau"
-            area="40 m²"
-            rooms={4}
-            postedBy="pixenbytayyub"
-            postedTime="2 days ago"
-          />
+          /> */}
+
+          <PaintingsGallery isAdmin={false} />
         </div>
       </div>
     </div>
